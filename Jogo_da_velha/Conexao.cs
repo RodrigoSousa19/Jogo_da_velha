e017addf4a3e31a6942bedd7ad14d3f543ee2824
Rodrigo Santos de Sousa <101156579+Rosenkat@users.noreply.github.com>
2022-05-13 22:35:24 -0300

    Banco de Dados ligado
    Banco de dados agora recebe as informações digitadas pelo usuário no Text Box do jogador 1, faltando implementar uma melhor coleta de dados para unir com o Jogador 2

---
+++ Jogo_da_velha/Conexao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_da_velha
+{
+   public class Conexao
+    {
+
+        SqlConnection Conecta = new SqlConnection();
+
+        public Conexao()
+        {
+            Conecta.ConnectionString = "Data Source=SQO-061\\SQLEXPRESS;Initial Catalog=JogoDaVelha;Integrated Security=True";
+        }
+
+        public SqlConnection Conectar()
+        {
+            if(Conecta.State == System.Data.ConnectionState.Closed)
+            {
+                Conecta.Open();
+            }
+            return Conecta;
+        }
+
+        public void Desconectar()
+        {
+            if (Conecta.State == System.Data.ConnectionState.Open)
+            {
+                Conecta.Close();
+            }
+        }
+    }
+}
